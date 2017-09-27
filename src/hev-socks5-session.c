5acab119000883d1d2dbb2f05070da832f1d2645
Heiher <r@hev.cc>
2017-09-27 15:16:37 +0800

    HevSocks5Session: Use HevTaskIO.

--- src/hev-socks5-session.c
+++ src/hev-socks5-session.c
@@ -20,6 +20,7 @@
 #include "hev-socks5-session.h"
 #include "hev-memory-allocator.h"
 #include "hev-task.h"
+#include "hev-task-io-socket.h"
 #include "hev-config.h"
 
 #define SESSION_HP	(10)
@@ -229,162 +230,16 @@ hev_socks5_session_run (HevSocks5Session *self)
 	hev_task_run (self->base.task, hev_socks5_session_task_entry, self);
 }
 
-static ssize_t
-task_socket_recv (int fd, void *buf, size_t len, int flags, HevSocks5Session *session)
-{
-	ssize_t s;
-	size_t size = 0;
-
-retry:
-	s = recv (fd, buf + size, len - size, flags & ~MSG_WAITALL);
-	if (s == -1 && errno == EAGAIN) {
-		hev_task_yield (HEV_TASK_WAITIO);
-		if (session->base.hp <= 0)
-			return -2;
-		goto retry;
-	}
-
-	if (!(flags & MSG_WAITALL))
-		return s;
-
-	if (s <= 0)
-		return size;
-
-	size += s;
-	if (size < len)
-		goto retry;
-
-	return size;
-}
-
-static ssize_t
-task_socket_sendmsg (int fd, const struct msghdr *msg, int flags, HevSocks5Session *session)
-{
-	ssize_t s;
-	size_t i, size = 0, len = 0;
-	struct msghdr mh;
-	struct iovec iov[msg->msg_iovlen];
-
-	mh.msg_name = msg->msg_name;
-	mh.msg_namelen = msg->msg_namelen;
-	mh.msg_control = msg->msg_control;
-	mh.msg_controllen = msg->msg_controllen;
-	mh.msg_flags = msg->msg_flags;
-	mh.msg_iov = iov;
-	mh.msg_iovlen = msg->msg_iovlen;
-
-	for (i=0; i<msg->msg_iovlen; i++) {
-		iov[i] = msg->msg_iov[i];
-		len += iov[i].iov_len;
-	}
-
-retry:
-	s = sendmsg (fd, &mh, flags & ~MSG_WAITALL);
-	if (s == -1 && errno == EAGAIN) {
-		hev_task_yield (HEV_TASK_WAITIO);
-		if (session->base.hp <= 0)
-			return -2;
-		goto retry;
-	}
-
-	if (!(flags & MSG_WAITALL))
-		return s;
-
-	if (s <= 0)
-		return size;
-
-	size += s;
-	if (size < len) {
-		for (i=0; i<mh.msg_iovlen; i++) {
-			if (s < iov[i].iov_len) {
-				iov[i].iov_base += s;
-				iov[i].iov_len -= s;
-				break;
-			}
-
-			s -= iov[i].iov_len;
-		}
-
-		mh.msg_iov += i;
-		mh.msg_iovlen -= i;
-
-		goto retry;
-	}
-
-	return size;
-}
-
-static ssize_t
-task_socket_sendto (int fd, const void *buf, size_t len, int flags,
-			const struct sockaddr *addr, socklen_t addr_len,
-			HevSocks5Session *session)
-{
-	ssize_t s;
-
-retry:
-	s = sendto (fd, buf, len, flags, addr, addr_len);
-	if (s == -1 && errno == EAGAIN) {
-		hev_task_yield (HEV_TASK_WAITIO);
-		if (session->base.hp <= 0)
-			return -2;
-		goto retry;
-	}
-
-	return s;
-}
-
 static int
-task_socket_splice (int fd_in, int fd_out, void *buf, size_t len,
-			size_t *w_off, size_t *w_left)
+socks5_session_task_io_yielder (HevTaskYieldType type, void *data)
 {
-	ssize_t s;
+	HevSocks5Session *self = data;
 
-	if (*w_left == 0) {
-		s = recv (fd_in, buf, len, 0);
-		if (s == -1) {
-			if (errno == EAGAIN)
-				return 0;
-			else
-				return -1;
-		} else if (s == 0) {
-			return -1;
-		} else {
-			*w_off = 0;
-			*w_left = s;
-		}
-	}
+	self->base.hp = SESSION_HP;
 
-	s = send (fd_out, buf + *w_off, *w_left, 0);
-	if (s == -1) {
-		if (errno == EAGAIN)
-			return 0;
-		else
-			return -1;
-	} else if (s == 0) {
-		return -1;
-	} else {
-		*w_off += s;
-		*w_left -= s;
-	}
+	hev_task_yield (type);
 
-	return *w_off;
-}
-
-static int
-task_socket_connect (int fd, const struct sockaddr *addr, socklen_t addr_len,
-			HevSocks5Session *session)
-{
-	int ret;
-retry:
-	ret = connect (fd, addr, addr_len);
-	if (ret == -1 && (errno == EINPROGRESS || errno == EALREADY)) {
-		hev_task_yield (HEV_TASK_WAITIO);
-		if (session->base.hp <= 0)
-			return -2;
-		goto retry;
-	}
-
-	return ret;
+	return (self->base.hp > 0) ? 0 : -1;
 }
 
 static int
@@ -411,7 +266,8 @@ socks5_do_connect (HevSocks5Session *self)
 	addr.sin_port = htons (hev_config_get_socks5_port ());
 
 	/* connect */
-	ret = task_socket_connect (self->remote_fd, (struct sockaddr *) &addr, addr_len, self);
+	ret = hev_task_io_socket_connect (self->remote_fd, (struct sockaddr *) &addr, addr_len,
+				socks5_session_task_io_yielder, self);
 	if (ret < 0)
 		return STEP_CLOSE_SESSION;
 
@@ -451,7 +307,8 @@ socks5_write_request (HevSocks5Session *self)
 	iov[1].iov_base = &socks5_r;
 	iov[1].iov_len = 10;
 
-	len = task_socket_sendmsg (self->remote_fd, &mh, MSG_WAITALL, self);
+	len = hev_task_io_socket_sendmsg (self->remote_fd, &mh, MSG_WAITALL,
+				socks5_session_task_io_yielder, self);
 	if (len <= 0)
 		return STEP_CLOSE_SESSION;
 
@@ -466,7 +323,8 @@ socks5_read_response (HevSocks5Session *self)
 	ssize_t len;
 
 	/* read socks5 auth method */
-	len = task_socket_recv (self->remote_fd, &socks5_auth, 2, MSG_WAITALL, self);
+	len = hev_task_io_socket_recv (self->remote_fd, &socks5_auth, 2, MSG_WAITALL,
+				socks5_session_task_io_yielder, self);
 	if (len <= 0)
 		return STEP_CLOSE_SESSION;
 	/* check socks5 version and auth method */
@@ -474,7 +332,8 @@ socks5_read_response (HevSocks5Session *self)
 		return STEP_CLOSE_SESSION;
 
 	/* read socks5 response */
-	len = task_socket_recv (self->remote_fd, &socks5_r, 10, MSG_WAITALL, self);
+	len = hev_task_io_socket_recv (self->remote_fd, &socks5_r, 10, MSG_WAITALL,
+				socks5_session_task_io_yielder, self);
 	if (len <= 0)
 		return STEP_CLOSE_SESSION;
 	/* check socks5 version, rep and atype */
@@ -487,60 +346,8 @@ socks5_read_response (HevSocks5Session *self)
 static int
 socks5_do_splice (HevSocks5Session *self)
 {
-	int splice_f = 1, splice_b = 1;
-	size_t w_off_f = 0, w_off_b = 0;
-	size_t w_left_f = 0, w_left_b = 0;
-	unsigned char buf_f[2048];
-	unsigned char buf_b[2048];
-
-	while (self->base.hp > 0) {
-		int no_data = 0;
-
-		self->base.hp = SESSION_HP;
-
-		if (splice_f) {
-			int ret;
-
-			ret = task_socket_splice (self->client_fd, self->remote_fd,
-						buf_f, 2048, &w_off_f, &w_left_f);
-			if (ret == 0) { /* no data */
-				/* forward no data and backward closed, quit */
-				if (!splice_b)
-					break;
-				no_data ++;
-			} else if (ret == -1) { /* error */
-				/* forward error and backward closed, quit */
-				if (!splice_b)
-					break;
-				/* forward error or closed, mark to skip */
-				splice_f = 0;
-			}
-		}
-
-		if (splice_b) {
-			int ret;
-
-			ret = task_socket_splice (self->remote_fd, self->client_fd,
-						buf_b, 2048, &w_off_b, &w_left_b);
-			if (ret == 0) { /* no data */
-				/* backward no data and forward closed, quit */
-				if (!splice_f)
-					break;
-				no_data ++;
-			} else if (ret == -1) { /* error */
-				/* backward error and forward closed, quit */
-				if (!splice_f)
-					break;
-				/* backward error or closed, mark to skip */
-				splice_b = 0;
-			}
-		}
-
-		/* single direction no data, goto yield.
-		 * double direction no data, goto waitio.
-		 */
-		hev_task_yield ((no_data < 2) ? HEV_TASK_YIELD : HEV_TASK_WAITIO);
-	}
+	hev_task_io_splice (self->client_fd, self->client_fd, self->remote_fd, self->remote_fd,
+				2048, socks5_session_task_io_yielder, self);
 
 	return STEP_CLOSE_SESSION;
 }
@@ -569,12 +376,14 @@ socks5_do_fwd_dns (HevSocks5Session *self)
 	iov[1].iov_len = self->dns_request_len;
 
 	/* send dns request */
-	len = task_socket_sendmsg (self->remote_fd, &mh, MSG_WAITALL, self);
+	len = hev_task_io_socket_sendmsg (self->remote_fd, &mh, MSG_WAITALL,
+				socks5_session_task_io_yielder, self);
 	if (len <= 0)
 		return STEP_CLOSE_SESSION;
 
 	/* read dns response length */
-	len = task_socket_recv (self->remote_fd, &dns_len, 2, MSG_WAITALL, self);
+	len = hev_task_io_socket_recv (self->remote_fd, &dns_len, 2, MSG_WAITALL,
+				socks5_session_task_io_yielder, self);
 	if (len <= 0)
 		return STEP_CLOSE_SESSION;
 	dns_len = ntohs (dns_len);
@@ -584,13 +393,15 @@ socks5_do_fwd_dns (HevSocks5Session *self)
 		return STEP_CLOSE_SESSION;
 
 	/* read dns response */
-	len = task_socket_recv (self->remote_fd, buf, dns_len, MSG_WAITALL, self);
+	len = hev_task_io_socket_recv (self->remote_fd, buf, dns_len, MSG_WAITALL,
+				socks5_session_task_io_yielder, self);
 	if (len <= 0)
 		return STEP_CLOSE_SESSION;
 
 	/* send dns response */
-	task_socket_sendto (self->client_fd, buf, len, 0,
-				(struct sockaddr *) &self->address, addr_len, self);
+	hev_task_io_socket_sendto (self->client_fd, buf, len, 0,
+				(struct sockaddr *) &self->address, addr_len,
+				socks5_session_task_io_yielder, self);
 
 	return STEP_CLOSE_SESSION;
 }
@@ -619,8 +430,6 @@ hev_socks5_session_task_entry (void *data)
 	hev_task_add_fd (task, self->client_fd, EPOLLIN | EPOLLOUT);
 
 	while (step) {
-		self->base.hp = SESSION_HP;
-
 		switch (step) {
 		case STEP_DO_CONNECT:
 			step = socks5_do_connect (self);
