3852bbc8ff6f20683a4177188b94f5424ce52660
Heiher <r@hev.cc>
2018-10-17 22:29:33 +0800

    HevMain: Sync to new coding style.

--- src/hev-main.c
+++ src/hev-main.c
@@ -19,36 +19,35 @@
 static void
 show_help (const char *self_path)
 {
-	printf ("%s CONFIG_PATH\n", self_path);
-	printf ("Version: %u.%u.%u\n", MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION);
+    printf ("%s CONFIG_PATH\n", self_path);
+    printf ("Version: %u.%u.%u\n", MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION);
 }
 
 int
 main (int argc, char *argv[])
 {
-	if (2 != argc) {
-		show_help (argv[0]);
-		return -1;
-	}
+    if (2 != argc) {
+        show_help (argv[0]);
+        return -1;
+    }
 
-	if (0 > hev_config_init (argv[1]))
-		return -2;
+    if (0 > hev_config_init (argv[1]))
+        return -2;
 
-	if (0 > hev_socks5_tproxy_init ())
-		return -3;
+    if (0 > hev_socks5_tproxy_init ())
+        return -3;
 
-	hev_socks5_tproxy_run ();
+    hev_socks5_tproxy_run ();
 
-	hev_socks5_tproxy_fini ();
+    hev_socks5_tproxy_fini ();
 
-	hev_config_fini ();
+    hev_config_fini ();
 
-	return 0;
+    return 0;
 }
 
 void
 quit (void)
 {
-	hev_socks5_tproxy_stop ();
+    hev_socks5_tproxy_stop ();
 }
-
