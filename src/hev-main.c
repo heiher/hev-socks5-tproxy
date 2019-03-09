2741189b490021f3f929cc330d43a4dd4465717f
Heiher <r@hev.cc>
2019-03-09 21:02:16 +0800

    HevMain: Run as a daemon with specific pid file.

--- src/hev-main.c
+++ src/hev-main.c
@@ -2,12 +2,13 @@
  ============================================================================
  Name        : hev-main.c
  Author      : Heiher <r@hev.cc>
- Copyright   : Copyright (c) 2017 everyone.
+ Copyright   : Copyright (c) 2017 - 2019 everyone.
  Description : Main
  ============================================================================
  */
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include "hev-main.h"
 #include "hev-task.h"
@@ -23,9 +24,30 @@ show_help (const char *self_path)
     printf ("Version: %u.%u.%u\n", MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION);
 }
 
+static void
+run_as_daemon (const char *pid_file)
+{
+    FILE *fp;
+
+    fp = fopen (pid_file, "w+");
+    if (!fp) {
+        fprintf (stderr, "Open pid file %s failed!\n", pid_file);
+        return;
+    }
+
+    if (daemon (0, 0)) {
+        /* ignore return value */
+    }
+
+    fprintf (fp, "%u", getpid ());
+    fclose (fp);
+}
+
 int
 main (int argc, char *argv[])
 {
+    const char *pid_file;
+
     if (2 != argc) {
         show_help (argv[0]);
         return -1;
@@ -36,6 +58,10 @@ main (int argc, char *argv[])
 
     if (0 > hev_socks5_tproxy_init ())
         return -3;
+
+    pid_file = hev_config_get_misc_pid_file ();
+    if (pid_file)
+        run_as_daemon (pid_file);
 
     hev_socks5_tproxy_run ();
 
