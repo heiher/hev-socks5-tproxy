e4fe3392ba296e7c68712520f449c9abe174c44f
hev <git@hev.cc>
2026-05-10 17:54:41 +0800

    HevConfig: Bump up to 2.11.0.

--- src/hev-config-const.h
+++ src/hev-config-const.h
@@ -11,8 +11,8 @@
 #define __HEV_CONFIG_CONST_H__
 
 #define MAJOR_VERSION (2)
-#define MINOR_VERSION (10)
-#define MICRO_VERSION (1)
+#define MINOR_VERSION (11)
+#define MICRO_VERSION (0)
 
 static const int UDP_BUF_SIZE = 1500;
 static const int UDP_POOL_SIZE = 512;
